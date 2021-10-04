9f2f175dbedeba0e536c26a26e748591b13d569f
Ari Kardasis <ari.kardasis@gmail.com>
2021-10-04 15:52:36 -0400

    change board

--- arduino/pullBoot.sh
+++ arduino/pullBoot.sh
@@ -1,3 +1,3 @@
 git pull &&
-arduino-cli compile --fqbn arduino:avr:diecimila:cpu=atmega328 runner &&
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:diecimila:cpu=atmega328 runner
+arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 runner &&
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328 runner
