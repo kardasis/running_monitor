34dfe845627080486ba2b3fd3d788ee76ffe8258
Ari Kardasis <ari.kardasis@gmail.com>
2021-10-05 19:08:34 -0400

    rpi zero setup and pyserver

--- arduino/pullBoot.sh
+++ arduino/pullBoot.sh
@@ -3,5 +3,5 @@
 # run once: arduino-cli lib install SparkFun_MPU-9250_9_DOF_IMU_Breakout
 
 git pull &&
-arduino-cli compile --fqbn arduino:avr:nano:cpu=atmega328 runner &&
-arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:nano:cpu=atmega328 runner
+arduino-cli compile --fqbn arduino:avr:diecimila:cpu=atmega328 runner &&
+arduino-cli upload -p /dev/ttyUSB0 --fqbn arduino:avr:diecimila:cpu=atmega328 runner
