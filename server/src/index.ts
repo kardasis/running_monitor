fa5a5d917a67be5df3165f003270fae997d15b1b
Ari Kardasis <ari.kardasis@gmail.com>
2021-10-07 10:49:27 -0400

    wip

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,10 @@
 import keypress from 'keypress'
 import SerialPort from 'serialport'
 import WebSocket from 'ws'
-import { Run } from './run'
 import { TICKS_PER_MILE, SECONDS_PER_HOUR } from './constants'
 
 let fake_speed = 1.0 // mph
 
-type ClientData = {
-  timestamp: number
-  tickCount: number
-  speed: number
-}
-
-let currentRun: Run
 let sendingData = false
 
 keypress(process.stdin);
@@ -33,7 +25,9 @@ process.stdin.resume();
 
 const wss = new WebSocket.Server({ port: 8081 });
 
+
 SerialPort.list().then((list) => {
+  console.log("hi")
   let port: SerialPort
   let parser: SerialPort.parsers.Readline
   if (list.some(portInfo => portInfo.path.includes('ttyUSB0'))) {
@@ -45,35 +39,42 @@ SerialPort.list().then((list) => {
   } else {
     console.log('failed to connect to arduino')
   }
-
-  wss.on('connection', function connection(ws): void {
-    console.log('connected to websocket')
-    ws.on('message', function incoming(data) {
-      const message = JSON.parse(data as string)
-      if (message.message === 'start') {
-        console.log('starting run')
-        currentRun = new Run(ws)
-      } else if (message.message === 'stop') {
-        console.log('stopping run')
-        ws.send(JSON.stringify({ message: 'stopping run' }))
-        currentRun.finish(!!parser)
-      }
-    })
-
-    if (parser) {
-      parser.on('data', data => {
-        const jsonData = JSON.parse(data)
-        if (currentRun && jsonData.data) {
-          currentRun.addDataPoint(jsonData)
-        }
-      })
-    } else {
-      if (!sendingData) {
-        sendFakeData()
-        sendingData = true
-      }
-    }
+  parser.on('data', data => {
+    console.log(data)
+    // const jsonData = JSON.parse(data)
+    // if (currentRun && jsonData.data) {
+    //   currentRun.addDataPoint(jsonData)
+    // }
   })
+
+  // wss.on('connection', function connection(ws): void {
+  //   console.log('connected to websocket')
+  //   ws.on('message', function incoming(data) {
+  //     const message = JSON.parse(data as string)
+  //     if (message.message === 'start') {
+  //       console.log('starting run')
+  //       currentRun = new Run(ws)
+  //     } else if (message.message === 'stop') {
+  //       console.log('stopping run')
+  //       ws.send(JSON.stringify({ message: 'stopping run' }))
+  //       currentRun.finish(!!parser)
+  //     }
+  //   })
+
+  //   if (parser) {
+  //     parser.on('data', data => {
+  //       const jsonData = JSON.parse(data)
+  //       if (currentRun && jsonData.data) {
+  //         currentRun.addDataPoint(jsonData)
+  //       }
+  //     })
+  //   } else {
+  //     if (!sendingData) {
+  //       sendFakeData()
+  //       sendingData = true
+  //     }
+  //   }
+  // })
 })
 
 function sendFakeData() {
