d7aa9ac440397723d0697a1fa1434de9b7f1c5db
Ari Kardasis <ari.kardasis@gmail.com>
2021-10-07 15:20:46 -0400

    sending data

--- server/src/aws.js
+++ server/src/aws.js
@@ -1,25 +1,26 @@
-import dotenv  from "dotenv"
+const AWS = require('aws-sdk')
+const dotenv = require('dotenv')
+
 dotenv.config()
 
-import AWS from 'aws-sdk'
-
-
-export function putToS3(runName, runData, bucketName) {
+module.exports = Object.freeze({
+  putToS3: function(runName, runData, bucketName) {
     const s3 = new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET
     })
     console.log('putting data to s3')
     const params = {
-        Bucket: bucketName,
-        Key: runName,
-        Body: JSON.stringify(runData)
+      Bucket: bucketName,
+      Key: runName,
+      Body: JSON.stringify(runData)
     };
 
     s3.upload(params, function (err, data) {
-        if (err) {
-            throw err;
-        }
-        console.log(`File uploaded successfully. ${data.Location}`);
+      if (err) {
+        throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
     })
-}
+  }
+})
--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,15 @@
 const keypress = require('keypress')
 const  SerialPort= require('serialport')
-const WebSocket = require('ws')
-
+const {putToS3} = require('./aws')
 const { TICKS_PER_MILE, SECONDS_PER_HOUR } = require('./constants')
+const dotenv = require('dotenv')
 
-let fake_speed = 1.0 // mph
+dotenv.config()
 
-let sendingData = false
+let state = 'standby'
+let ticks
+let startTime
+let lastTickTime
 
 keypress(process.stdin);
 process.stdin.on('keypress', function (ch, key) {
@@ -14,18 +17,17 @@ process.stdin.on('keypress', function (ch, key) {
     console.log("bye bye");
     process.exit();
   } else if (key && key.name == 'up') {
-    fake_speed += .1
+  putToS3(
+    'run-' + Date.now() + '.json', 
+    {ticks: [100, 420, 732], startTime: Date.now()}, 
+    'ak-sandbox')
   } else if (key && key.name == 'down') {
-    fake_speed -= .1
+    // fake_speed -= .1
   }
-  console.log(`fake_speed: ${fake_speed}`)
 });
 
 process.stdin.setRawMode(true);
 process.stdin.resume();
-
-const wss = new WebSocket.Server({ port: 8081 });
-
 
 SerialPort.list().then((list) => {
   let port
@@ -39,51 +41,68 @@ SerialPort.list().then((list) => {
   } else {
     console.log('failed to connect to arduino')
   }
-  parser.on('data', data => {
-    console.log(data)
-    // const jsonData = JSON.parse(data)
-    // if (currentRun && jsonData.data) {
-    //   currentRun.addDataPoint(jsonData)
-    // }
-  })
-
-  // wss.on('connection', function connection(ws): void {
-  //   console.log('connected to websocket')
-  //   ws.on('message', function incoming(data) {
-  //     const message = JSON.parse(data as string)
-  //     if (message.message === 'start') {
-  //       console.log('starting run')
-  //       currentRun = new Run(ws)
-  //     } else if (message.message === 'stop') {
-  //       console.log('stopping run')
-  //       ws.send(JSON.stringify({ message: 'stopping run' }))
-  //       currentRun.finish(!!parser)
-  //     }
-  //   })
-
-  //   if (parser) {
-  //     parser.on('data', data => {
-  //       const jsonData = JSON.parse(data)
-  //       if (currentRun && jsonData.data) {
-  //         currentRun.addDataPoint(jsonData)
-  //       }
-  //     })
-  //   } else {
-  //     if (!sendingData) {
-  //       sendFakeData()
-  //       sendingData = true
-  //     }
-  //   }
-  // })
+  parser.on('data', handleData)
 })
 
-function sendFakeData() {
-  const tickTime = 1000 * SECONDS_PER_HOUR / fake_speed / TICKS_PER_MILE
-  setTimeout(() => {
-    if (currentRun) {
-      currentRun.sendFakeData(fake_speed)
-    }
-    sendFakeData()
-  }, tickTime)
+function handleData(data) {
+  console.log(data, state)
+  if (state == 'standby') {
+    state = 'running'
+    ticks = []
+    startTime = Date.now()
+  } 
+  ticks.push(data)
+  lastTickTime = Date.now()
 }
+
+function endRun() {
+  console.log('finalizing run')
+  uploadRun()
+  state = 'standby'
+}
+
+function uploadRun() {
+  console.log(process.env.BUCKET_NAME)
+  putToS3(
+    'run-' + startTime + '.json', 
+    {ticks, startTime}, 
+    process.env.BUCKET_NAME)
+}
+
+var intervalId = setInterval(activityCheck, 1000);
+function activityCheck() {
+  if (state == 'running') {
+    const now = Date.now()
+    if (now-lastTickTime > 2000) {
+      endRun()
+    }
+  }
+}
+
+
+// const wss = new WebSocket.Server({ port: 8081 });
+// wss.on('connection', function connection(ws): void {
+//   console.log('connected to websocket')
+//   ws.on('message', function incoming(data) {
+//     const message = JSON.parse(data as string)
+//     if (message.message === 'start') {
+//       console.log('starting run')
+//       currentRun = new Run(ws)
+//     } else if (message.message === 'stop') {
+//       console.log('stopping run')
+//       ws.send(JSON.stringify({ message: 'stopping run' }))
+//       currentRun.finish(!!parser)
+//     }
+//   })
+
+//   if (parser) {
+//     parser.on('data', data => {
+//       const jsonData = JSON.parse(data)
+//       if (currentRun && jsonData.data) {
+//         currentRun.addDataPoint(jsonData)
+//       }
+//     })
+//   }
+// })
+
 
