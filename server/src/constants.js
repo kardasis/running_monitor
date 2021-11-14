2428de2fcbc6384c0bf4c3cbcea6d98071f13dc1
Ari Kardasis <ari.kardasis@gmail.com>
2021-11-14 13:16:58 -0500

    ticks per mile

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-  TICKS_PER_MILE : 5280 * 2, 
+  TICKS_PER_MILE : 5280 * (6/3.12), 
   MILLIS_PER_HOUR : 60 * 60 * 1000,
   DEBOUNCE_TIME : 20,
   SPEED_SMOOTHING : 0.5,
